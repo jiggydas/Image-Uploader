b22a49f32821e2c5d3d09805f8df9e2ad8195a5e
Jigyansoo Das <34629369+jiggydas@users.noreply.github.com>
2020-11-09 15:55:51 +0530

    Update app.component.ts

--- task/src/app/app.component.ts
+++ task/src/app/app.component.ts
@@ -32,11 +32,12 @@ export class AppComponent {
       });
       this.uploadService.uploadDesc(this.desc,file.size,file.type,file.name).subscribe(response2=>{
         console.log(response2);
+        this.showImgError=false;
+        this.showDescError=false;
+        this.failureMessage=false;
+        this.successMessage=true;
       });
-      this.showImgError=false;
-      this.showDescError=false;
-      this.failureMessage=false;
-      this.successMessage=true;
+
     } else {
       if(file.size>500000)
       this.showImgError = true;
